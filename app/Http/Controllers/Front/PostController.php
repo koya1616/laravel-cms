77ba50d2fae9117c1b9a63f05ba38df42aef1b47
koya1616 <87183507+koya1616@users.noreply.github.com>
2021-11-30 21:59:05 +0900

    first commit

---
+++ app/Http/Controllers/Front/PostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // 公開・新しい順に表示
+        $posts = Post::publicList();
+
+        return view('front.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePostRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(int $id)
+    {
+        $post = Post::publicFindById($id);
+
+        return view('front.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePostRequest  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePostRequest $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
---
+++ app/Models/Post.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'body', 'is_public', 'published_at'
+    ];
+
+    protected $casts = [
+        'is_public' => 'bool',
+        'published_at' => 'datetime'
+    ];
+
+    // 公開のみ表示
+    public function scopePublic(Builder $query)
+    {
+        return $query->where('is_public', true);
+    }
+
+    // 公開記事一覧取得
+    public function scopePublicList(Builder $query)
+    {
+        return $query
+            ->public()
+            ->latest('published_at')
+            ->paginate(10);
+    }
+
+    // 公開記事をIDで取得
+    public function scopePublicFindById(Builder $query, int $id)
+    {
+        return $query->public()->findOrFail($id);
+    }
+
+    // 公開日を年月日で表示
+    public function getPublishedFormatAttriute()
+    {
+        return $this->published_at->format('Y年m月d日');
+    }
+}
